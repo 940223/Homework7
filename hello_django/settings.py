346365e52ba5d94008a9b8f7eb00aab294060533
張翔 <940223@gmail.com>
2021-05-28 13:37:02 +0800

    settings

--- hello_django/settings.py
+++ hello_django/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--)hn=ydk5$%(b(m9ps3+(8cb8fp+ivgzst#0xmfvsjhhiu(p$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['djang0test.herokuapp.com',"127.0.0.1"]
+ALLOWED_HOSTS = ['djang01.herokuapp.com',"127.0.0.1"]
 
 
 # Application definition
@@ -124,6 +124,8 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 LOGGING = {
 'version': 1,
